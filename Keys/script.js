e87264e10d4f2ff2d849fcbdeb5dd200d79e4e96
Slogllykop <slogllykop07@gmail.com>
2022-11-09 20:06:24 +0530

    first mini project initial

---
+++ Keys/script.js
@@ -0,0 +1,56 @@
+"use strict";
+
+const w = document.querySelector('.key-w');
+const a = document.querySelector('.key-a');
+const s = document.querySelector('.key-s');
+const d = document.querySelector('.key-d');
+const LShift = document.querySelector('.key-shift');
+const space = document.querySelector('.key-space');
+
+(function(){
+    document.addEventListener('keypress', function (e) {
+        console.log(e);
+    });
+})();
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'w') {
+    w.classList.add('active');
+  }
+  if (e.key === 'a') {
+    a.classList.add('active');
+  }
+  if (e.key === 's') {
+    s.classList.add('active');
+  }
+  if (e.key === 'd') {
+    d.classList.add('active');
+  }
+  if (e.shiftKey) {
+    LShift.classList.add('active');
+  }
+  if (e.key === ' ') {
+    space.classList.add('active');
+  }
+});
+
+document.addEventListener('keyup', function (e) {
+  if (e.key === 'w') {
+    w.classList.remove('active');
+  }
+  if (e.key === 'a') {
+    a.classList.remove('active');
+  }
+  if (e.key === 's') {
+    s.classList.remove('active');
+  }
+  if (e.key === 'd') {
+    d.classList.remove('active');
+  }
+  if (!e.shiftKey) {
+    LShift.classList.remove('active');
+  }
+  if (e.key === ' ') {
+    space.classList.remove('active');
+  }
+});
